402eacf3abf6773f7c41f9ebd620b53e6b292c09
Kishan <kishanveersingh2704@gmail.com>
2026-01-29 22:26:16 +0000

    streamlit app ui

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import joblib
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+# Load Model
+@st.cache_resource
+def load_model():
+    return joblib.load('sentiment_mnb_model.pkl')
+
+model = model_load()
