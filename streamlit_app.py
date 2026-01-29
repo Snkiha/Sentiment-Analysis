5f6b3b6705dd2d02c796b53ba27d5a611fd0285c
Kishan <kishanveersingh2704@gmail.com>
2026-01-29 22:36:12 +0000

    web title

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,6 @@ def load_model():
     return joblib.load('sentiment_mnb_model.pkl')
 
 model = load_model()
+
+st.title("What's the Review: Sentiment Analysis")
+st.markdown("Enter a **Review** of your choice and it will be output as ** Positive** or **Negative** review.")
